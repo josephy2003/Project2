99d493bbff312135175ca09f00d9d5779c86f1d5
_ixmnxbdy111 <65754413+josephy2003@users.noreply.github.com>
2023-11-12 16:56:35 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+import random
+
+# class Guitars:
+#     def __init__(self, brand, model, type):
+#         self.brand = brand
+#         self.model = model
+#         self.type = type
+#
+#     def buildGuitar(self):
---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+from config import *
+
+header = open(file='fileoverview.txt',mode="r")
+
+
+
+
+def main():
+    prompt = int(input("PICK A CARD, ANY CARD ;): \n\n"))
+
+
+
+
+
+
+
+main()
